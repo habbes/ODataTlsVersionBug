92b41009b15f2bef1312c5e5284c77c242ebe09e
Clement Habinshuti <clhabins@microsoft.com>
2020-01-07 12:27:16 +0300

    Add Odata client

---
+++ SampleClient/SampleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using SampleService.Models;
+using Default;
+
+namespace SampleClient
+{
+    class Program
+    {
+        static Uri uri = new Uri("https://localhost:5001/odata");
+        static async Task Main(string[] args)
+        {
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
+            var context = new Container(uri);
+            var people = await context.People.ExecuteAsync();
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.Name);
+            }
+        }
+    }
+}
